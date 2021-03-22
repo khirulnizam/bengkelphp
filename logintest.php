769b01b8f56c07325be828aaa990dae5ada1231a
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2021-03-22 14:28:29 +0800

    testboot - logintest.php

---
+++ logintest.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <title>Login</title>
+  </head>
+  <body>
+      <div class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <h1>Borang log masuk </h1>
+                <form>
+                    Nama pengguna
+                    <input type="text" name="username" 
+                    placeholder="Nama pengguna"
+                    class="form-control">
+                    Kata laluan
+                    <input type="password" name="password" 
+                    class="form-control">
+                    Masa
+                    <input type="time" name="time" 
+                    class="form-control">
+                    Tarikh
+                    <input type="date" name="date" 
+                    class="form-control">
+                    <input type="submit" value="Login" 
+                    class="btn btn-primary">
+                </form>
+                
+            </div>
+            <div class="col-sm-4 bg-warning">
+
+            </div>
+        </div> 
+    </div>
+  </body>
+</html>
