f4b5f5823faa40a92b22b5656cf5e652c0cf159a
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2021-03-24 11:15:42 +0800

    update menu

--- menu.php
+++ menu.php
@@ -1,8 +1,10 @@
 <!-- menu.php
      YOUR MENU HERE -->
-    <h4>Menu selenggaraan rekod</h4>
-    <a href="searchadmin.php" class="btn btn-primary btn-block">Carian pelajar</a>
-    <a href="forminsert.php" class="btn btn-primary btn-block">Pelajar baharu</a>
+    <h4>Menu selenggaraan rekod aduan</h4>
+    <a href="1formsearchaduan.php" class="btn btn-primary btn-block">
+    Carian aduan</a>
+    <a href="1formaduan.php" class="btn btn-primary btn-block">
+    Masukkan aduan</a>
     <!-- footer.template.php -->
     <a href="formsearch-tempahan.php"
     class="btn btn-primary btn-block">
